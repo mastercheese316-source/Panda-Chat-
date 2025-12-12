4d6f1e936eeec848d914ec5ea651be27c9450aab
mastercheese316-source <mastercheese316@gmail.com>
2025-12-12 13:23:42 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const express = require('express');
 const http = require('http');
+const { Server: WebSocketServer } = require('ws');
 const app = express();
 app.use(express.json());
 app.use(express.static('src'));
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'src', 'index.html')));
 const server = http.createServer(app);
-const ws = new WebSocket.Server({ server });
-server.on('upgrade', (req, socket, head) => { ws.handleUpgrade(req, socket, head, socket => { socket.write(head); ws.emit('connection', socket, req); }); });
+const wss = new WebSocketServer({ server });
+server.on('upgrade', (req, socket, head) => { wss.handleUpgrade(req, socket, head, (ws) => { wss.emit('connection', ws, req); }); });
 server.listen(8080, () => console.log('Listening on port 8080'));
 
