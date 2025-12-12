1b8635cbcd5e032f347061ab4615e64cd839ef87
mastercheese316-source <mastercheese316@gmail.com>
2025-12-12 14:12:01 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
-server.listen(8080, () => console.log('Listening on port 8080'));
-
- 
+js const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static('src'));
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'src', 'index.html')); });
+const port = process.env.PORT || 8080; 
+app.listen(port, () => console.log(`Listening on port ${port}`));
