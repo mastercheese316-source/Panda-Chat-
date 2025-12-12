bdee56324f3b3c2bfe2ae87799aad9ef2b24d946
mastercheese316-source <mastercheese316@gmail.com>
2025-12-11 17:03:35 -0700

    Update serverdot.js

--- src./serverdot.js
+++ src./serverdot.js
@@ -1,1 +1,7 @@
-
+const path = require('path');
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.static('.'));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'index.html')));
+app.listen(8080, () => console.log('Listening on port 8080'));
