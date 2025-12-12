cdb682a44347019db95a23cb98ff3a452a41df1f
mastercheese316-source <mastercheese316@gmail.com>
2025-12-12 12:48:09 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
 const app = express();
 app.use(express.json());
-app.use(express.static('.'));
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'index.html')));
-app.listen(8080, () => console.log('Listening on port 8080'));
+app.use(express.static('src'));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'src', 'index.html')));
+const server = http.createServer(app);
+const ws = new WebSocket.Server({ server });
+server.on('upgrade', (req, socket, head) => { ws.handleUpgrade(req, socket, head, socket => { socket.write(head); ws.emit('connection', socket, req); }); });
+server.listen(8080, () => console.log('Listening on port 8080'));
+
